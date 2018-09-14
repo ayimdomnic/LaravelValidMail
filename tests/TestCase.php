75437aacf7ab488048de3fd57a29f5f12eee36ec
Odhiambo Dorminic <ayimdomnic@gmail.com>
2018-09-14 11:19:33 +0300

    base Orchestra TestBench Test asserts Psr-4

---
+++ tests/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ayim
+ * Date: 9/14/18
+ * Time: 11:16 AM
+ */
+
+namespace Ayim\LaravelValidMail\Tests;
+
+
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return ['Ayim\LaravelValidMail\LaravelValidMailServiceProvider'];
+    }
+
+    /**
+     * Load package alias
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'ValidMail' => 'Ayim\LaravelValidMail\Facades\ValidMail'
+        ];
+    }
+
+}
